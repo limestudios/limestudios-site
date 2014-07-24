5aac7ecced4f1b5658d870ab290a67fdb88f1238
Liam Craver <studios.lime@gmail.com>
2014-07-24 01:21:16 -0500

    Update JS for work page.

--- src/js/page-specific/work-belt.js
+++ src/js/page-specific/work-belt.js
@@ -3,13 +3,26 @@
   
     $('a.project').click(function(event) {
       event.preventDefault();
+      var class = $(this).attr('href');
       $('.work-belt').css('left','-100%');
-      $('.work-container').show();
+      $('.thumb-container').css('opacity','0');
+      $('.work-container div.' + class).show();
+      $('.work-container article.' + class).show();
+      $('.work-container').css('opacity','1');
+      $('html, body').animate({
+        screenTop: ($("article").first().offset().top)
+      }, 1000);
     });
 
-    $('.work-return').click(function() {
+    $('.work-return').click(function(event) {
+      event.preventDefault();
+      var class = $(this).attr('back');
       $('.work-belt').css('left','0%');
-      $('.work-container').hide(125);
+      $('.thumb-container').css('opacity','1');
+      $('.work-container div.' + class).hide(500);
+      $('.work-container article.' + class).hide(500);
+      $('.work-container').css('opacity','0');
+      console.log('.work-container article.' + class);
     });
 
   });
