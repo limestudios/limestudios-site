468d58d4d7a266df7fd94242d42b443466792278
Liam Craver <studios.lime@gmail.com>
2015-07-17 18:10:47 -0500

    assemble 2

---
+++ new/Gruntfile.js
@@ -0,0 +1,140 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// '<%= config.src %>/templates/pages/{,*/}*.hbs'
+// use this if you want to match all subfolders:
+// '<%= config.src %>/templates/pages/**/*.hbs'
+
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
+
+    watch: {
+      assemble: {
+        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
+        tasks: ['build-new-content']
+      },
+      styles: {
+        files: ['<%= config.src %>/assets/css/**/*.less'], // which files to watch
+        tasks: ['build-new-styles']
+      },
+      js: {
+        files: ['<%= config.src %>/assets/js/**/*.js'], // which files to watch
+        tasks: ['copy:js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/**/*.html',
+          '<%= config.dist %>/assets/css/*.css'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: '<%= config.src %>/templates/partials/*.hbs',
+          plugins: ['assemble-contrib-permalinks','assemble-contrib-sitemap'],
+          permalinks: {
+            structure: ':basename/index.html'
+          }
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/content/pages/*.hbs']
+        }
+      }
+    },
+
+    /*copy: {
+      bootstrap: {
+        expand: true,
+        cwd: 'bower_components/bootstrap/dist/',
+        src: '**',
+        dest: '<%= config.dist %>/assets/'
+      },
+      theme: {
+        expand: true,
+        cwd: 'src/assets/',
+        src: '**',
+        dest: '<%= config.dist %>/assets/css/'
+      }
+    },*/
+    
+    less: {
+      development: {
+        options: {
+          compress: false,
+        },
+        files: {
+          "<%= config.dist %>/assets/css/styles.css": "<%= config.src %>/assets/css/*.less"
+        }
+      }
+    },
+    
+    /*
+    ** grunt dev to start task
+    ** then go to localhost:1313
+    */
+    autoprefixer: {
+      options: {
+        browsers: ['last 5 version' ]
+      },
+      styles: {
+        expand: true,
+        flatten: true,
+        src: '<%= config.dist %>/assets/css/*.css',
+        dest: '<%= config.dist %>/assets/css/'
+      },
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= config.dist %>/**/*.{html,xml,css}']
+
+  });
+
+  grunt.registerTask('server', ['build', 'connect:livereload', 'watch']);
+  
+  grunt.registerTask('styles-new', ['newer:less', 'newer:autoprefixer']);
+  grunt.registerTask('styles', ['less', 'autoprefixer']);
+  
+  grunt.registerTask('build-new-styles', ['styles-new']);
+  grunt.registerTask('build-new-content', ['newer:assemble']);
+  grunt.registerTask('build-new', ['styles-new', 'newer:assemble']);
+  grunt.registerTask('build', ['clean', 'styles', 'assemble']);
+
+};
